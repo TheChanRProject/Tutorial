3f168ef444fe04609d24d278a770e556f31d418d
Rishov Chatterjee <rchatter@Rishovs-MacBook-Air.local>
2018-03-25 16:16:07 -0700

    Adding some files

---
+++ js/script.js
@@ -0,0 +1,3 @@
+document.getElementById('button').onclick = () => {
+  document.getElementById('display').innerHTML = parseInt(document.getElementById('numberTwo')) + parseInt(document.getElementById('numberOne'))
+}
